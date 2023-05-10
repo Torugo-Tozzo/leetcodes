a6650d95815612b6acc56d17eec35b817c6435c4
Victor H. T. Filho <114814350+Torugo-Tozzo@users.noreply.github.com>
2023-05-10 07:56:06 -0300

    Create prefix_compare.js
    letcode challenge

---
+++ prefix_compare.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function(strs) {
+    for(let i = 0; i < strs.length; i++){
+        for(let j = 0; j < strs[i].length; ){
+            if(){
+                //compara o string
+                //while strs[i][j] == strs[i+1][j] => stringResult.push(strs[i][j]) 
+                //é isso eu acho
+            }
+        }
+    }
+};
